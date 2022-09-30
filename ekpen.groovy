2acbf430bc1f2e2e62992bd3f35d7ea17fb100b1
Ekpen24 <111384393+Ekpen24@users.noreply.github.com>
2022-09-30 02:15:20 -0400

    Create ekpen.groovy

---
+++ ekpen.groovy
@@ -0,0 +1,45 @@
+@Library('LandmarkSS-sharedlibs') _
+pipeline {
+agent any
+stages{
+stage('CheckoutCode')
+{
+steps
+{
+git branch: 'development', credentialsId: '6773dc34-a3fd-44ec-b1ee-
+3d2f0aa9f346', url: https://github.com/LandmakTechnology/maven-web-
+application.git'}
+}
+}
+stage("Build"){
+steps{
+stages("Build")
+}
+}
+stage("Execute SonarQube Report"){
+steps{
+stages("SonarQube Report")
+stage("Upload Artifacts Into Nexus"){
+steps{
+stages("Upload Into Nexus")
+}
+}
+}// Stages Close
+} // Pipeline Close
+To get the build parameters...
+parameters {
+string(name: 'GoalName', defaultValue: 'Package', description: 'Pass the Maven Goals')
+choice(name: 'BranchName', choices: ['master', 'development', 'stage'], description:
+'Pick the Branch Name.')
+
+}
+
+stages{
+stage('CheckouCode')
+{
+steps
+{
+git branch: "${params.BranchName}", credentialsId: '6773dc34-a3fd-44ec-b1ee-
+3d2f0aa9f346', url: ' https://github.com/LandmakTechnology/maven-web-application.git'
+}
+}
